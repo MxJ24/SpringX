1753152455b67c4f4d1347b551877e2f04fa6b26
MxJ24 <stylr21@gmail.com>
2014-08-26 22:40:28 +0800

    test amend a commit once again

--- src/com/mxJ/bean/PermissionIntercept.java
+++ src/com/mxJ/bean/PermissionIntercept.java
@@ -10,44 +10,39 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
-/*
- * @desc 权限拦截切面
- */
 @Aspect
-//将该类声明为一个切面
 @Component
-//将该类声明为一个组件 这两个注解必须一起出现
 public class PermissionIntercept{
-	@Pointcut("execution(* com.mxJ.service.UserService.addUser(com.mxJ.bean.User))")//声明一个切入点 一个切入点必须依附在一个空方法上
-	public void unnamed(){//这个方法的名字就是该切入点的名字 当前切入点的名字是 unnamed()
-		
+	@Pointcut("execution(* com.mxJ.service.UserService.addUser(com.mxJ.bean.User))")
+	public void unnamed(){
+
 	}
-	
-	 @Before("unnamed() && args(user)") //前置通知 引用切入点 unnamed(),并且拦截的方法的参数必须是User类;
+
+	 @Before("unnamed() && args(user)")
 	 public void doAccessCheck(User user){
 			System.out.println("doAccessCheck Method!!!");
 	 }
-	 
-	 @AfterReturning(pointcut="unnamed()",returning="str")//后置通知,并且拿到应用后置通知方法返回的值
+
+	 @AfterReturning(pointcut="unnamed()",returning="str")
 	 public void after(String str){
 	    	System.out.println("After Returning Method !!!");
 	 }
-	 
-	 @After("unnamed()")//最终通知
+
+	 @After("unnamed()")
 	 public void reAfter(){
-	    	System.out.println("最终通知");
+	    	System.out.println("");
 	 }
-	 
-	 @AfterThrowing(pointcut="unnamed()",throwing="exc")//例外通知,并将拦截到的Exception作为参数传递给 exception0 方法作为参数
+
+	 @AfterThrowing(pointcut="unnamed()",throwing="exc")
 	 public void exception0(Exception exc){
-	    	System.out.println("我是例外通知");
+	    	System.out.println("");
 	 }
-	 
+
 	 @Around("unnamed()")
 	 public Object methodName(ProceedingJoinPoint pjp) throws Throwable{
 	    	System.out.println("Around In!!!");
 	        Object result=pjp.proceed();
-	        System.out.print++++++ln("Around Out!!!");
+	        //add a amend commit
 	      return result;
 	}
 }
